c173e301eccf7360b0e20e81f34bbbbba7ccda20
Luka Prolic <luka.prolic@marktplatz-mittelstand.de>
2025-02-05 08:50:29 +0100

    MVC Struktur ist aufgesetzt

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+use library\Application;
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+try {
+    spl_autoload_register(function($class) {
+        $path = './'.lcfirst(str_replace('\\', '/',$class)).'.php';
+        require $path;
+    });
+
+    $app = new Application();
+    $app->dispatch($_POST);
+
+} catch (\Exception $e) {
+    var_dump($e);
+    exit(1);
+}
+exit;
---
+++ library/View.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace library;
+
+class View
+{
+    public function render(string $view, array $data, string $layout = 'default') {
+        /**
+         * Prüfen, ob es ein View-Template mit dem gegebenen Namen gibt
+         */
+        if (file_exists(Application::TEMPLATE_BASE_PATH.'views/'.$view.'.phtml')) {
+            /**
+             * Prüfen, ob es das gegebene Layout Verzeichnis gibt
+             */
+            if (is_dir(Application::TEMPLATE_BASE_PATH.'layouts/'.$layout)) {
+                /**
+                 * Gegebene Layout und View Dateien einbinden
+                 */
+                require Application::TEMPLATE_BASE_PATH.'layouts/'.$layout.DIRECTORY_SEPARATOR.'header.phtml';
+                require Application::TEMPLATE_BASE_PATH.'views/'.$view.'.phtml';
+                require Application::TEMPLATE_BASE_PATH.'layouts/'.$layout.DIRECTORY_SEPARATOR.'footer.phtml';
+            }
+        }
+    }
+}
