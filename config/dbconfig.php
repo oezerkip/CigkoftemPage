1a3197fd08439e1ed66c4de3a5922ce164642252
Luka Prolic <luka.prolic@marktplatz-mittelstand.de>
2025-02-10 12:11:41 +0100

    Befehl Tracking-Out rückgängig gemacht

---
+++ config/dbconfig.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'db' => [
+        'host' => 'localhost',
+        'user' => 'luka',
+        'password' => 'lukajana0406',
+        'dbname' => 'cigkoftemDB'
+    ],
+    'layout' => [
+        'default_layout' => 'default'
+    ]
+];
